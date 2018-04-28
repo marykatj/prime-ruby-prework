447837b7558144e4b594f55364d2006a5aa2814c
Mary-Kate James <marykatj@gmail.com>
2018-04-28 20:43:06 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,6 +1,6 @@
 def prime?(integer)
   array = []
-  if integer < 0 || integer == 0
+  if integer < 0 || integer == 0 || integer == 1
     return false
   elsif Math.sqrt(integer).floor.downto(2).each {|i| return false if integer % i == 0 }
     return true
